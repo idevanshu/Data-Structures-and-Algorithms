c14a1fa989c8da111e2295ed8f937a50b6281f7d
Devanshu <devanshupanigrahi2004@gmail.com>
2023-07-03 10:59:25 +0530

    Solved leetcode question no. 1859

---
+++ Python/Buddy String.py
@@ -0,0 +1,18 @@
+def buddyStrings(s, goal):
+    if len(s) != len(goal):return False
+    if s == goal and len(set(s)) < len(s):return True
+
+    differences = []
+
+    for i in range(len(s)):
+        if s[i] != goal[i]:
+            differences.append([s[i],goal[i]])
+
+    if len(differences) == 2 and differences[0] == differences[-1][::-1]:
+        return True
+    
+    return False
+
+
+
+print(buddyStrings("aaaaaaabc", "aaaaaaacb"))
