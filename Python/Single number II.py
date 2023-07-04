131a59aed989b88a40f70ec18c0718fa05cb0b81
Devanshu <devanshupanigrahi2004@gmail.com>
2023-07-04 12:08:04 +0530

    Solved leetcode question no. 137

---
+++ Python/Single number II.py
@@ -0,0 +1,11 @@
+def singleNumber(nums):
+    nums.sort()
+    i = 0
+    while i < len(nums) - 1:
+        if nums[i] != nums[i+1]:
+            return nums[i]
+        i += 3
+    return nums[-1]
+    
+print(singleNumber([0, 1, 0, 1, 0, 1, 99])) 
+
